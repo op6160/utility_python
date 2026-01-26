9bfd169b2837e3fe457011398f3c0942c0776c97
op6160 <sagvd01@gmail.com>
2026-01-26 13:44:47 +0900

    feat: Add a new feature install_lib: Install python library on pip.

--- src/core/py_lib/python_lib.py
+++ src/core/py_lib/python_lib.py
@@ -1,5 +1,7 @@
 import sys
 import os
+import subprocess
+import importlib.util
 
 def get_python_filename(depth: int = 1):
     try:
@@ -12,3 +14,20 @@ def get_python_codeline(depth: int = 1):
         return sys._getframe(depth).f_lineno
     except (ValueError, AttributeError):
         return 0
+
+def install_lib(lib_name:str, install_name:str) -> bool:
+    result = False
+    spec = importlib.util.findspec(lib_name)
+    if spec is None:
+        print(f"{lib_name} not found. Installing...")
+        result = subprocess.check_call([sys.executable, "-m", "pip", "install", install_name])
+        try:
+            importlib.import_module(lib_name)
+            print(f"{lib_name} is installed successfully...")
+            result = True
+        except ImportError:
+            print(f"Failed to import {lib_name}...")
+
+    else:
+        print(f"{lib_name} is installed already...")
+    return result
